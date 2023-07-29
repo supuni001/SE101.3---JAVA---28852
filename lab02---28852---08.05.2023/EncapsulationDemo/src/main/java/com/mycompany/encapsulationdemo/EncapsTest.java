d447ebd47c070e38c926076d5cd11c8cdf2ac051
supuni001 <136358795+supuni001@users.noreply.github.com>
2023-07-30 01:48:02 +0530

    Add files via upload

---
+++ lab02---28852---08.05.2023/EncapsulationDemo/src/main/java/com/mycompany/encapsulationdemo/EncapsTest.java
@@ -0,0 +1,8 @@
+package com.mycompany.encapsulationdemo;
+public class EncapsTest{
+    public static void main(String args[]){
+         EncapsulationDemo obj = new EncapsulationDemo();
+         obj.setEmpName("Mario");
+         System.out.println("Employee Name: " + obj.getEmpName());
+    } 
+}
---
+++ lab02---28852---08.05.2023/EncapsulationDemo/src/main/java/com/mycompany/encapsulationdemo/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.mycompany.encapsulationdemo;
+public class EncapsulationDemo{
+    private String empName;
+
+    //Getter and Setter methods
+
+    public String getEmpName(){
+        return empName;
+    }
+
+    public void setEmpName(String newValue){
+        empName = newValue;
+    }
+}
